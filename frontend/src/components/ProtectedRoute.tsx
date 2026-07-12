7b965e27901103c374a466c8fd5bc1aa6503b30f
Myheadhurts16 <jamessdemesa@gmail.com>
2026-07-12 12:10:52 +0800

    Remove unused isError variable from ProtectedRoute

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useMe } from "@/hooks/useAuth";
 
 export default function ProtectedRoute() {
-  const { data: user, isLoading, isError } = useMe();
+  const { data: user, isLoading } = useMe();
 
   if (isLoading) {
     return (
